f93bbe9a9f69df8265f426a7c086b4eb53126a7d
Victoria Morgan <vmorgan1@companieshouse.gov.uk>
2022-01-20 12:15:52 +0000

    add two numbers

---
+++ src/main/java/com/example/addtwonumbers/AddTwoNumbers.java
@@ -0,0 +1,16 @@
+
+package com.example.addtwonumbers;
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public AddTwoNumbers() {
+    }
+
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 37;
+        int sum = num1 + num2;
+        System.out.println("Sum of these numbers: " + sum);
+    }
+}
+
